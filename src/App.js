3dee756e1b135b5dd1c4b9a79d2289b0988309b6
MartinStoev00 <martinstoev00@gmail.com>
2021-02-14 23:54:45 +0100

    finsished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as images from "./names.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <h1>Вики Колаж</h1>
+        <h4>Кликни снимката за да видиш изненада </h4>
+      </div>
+      <div className="container">
+        <div className="cards">
+          {images.default
+            .sort(() => 0.5 - Math.random())
+            .map((image) => {
+              return (
+                <div className="card" key={image.name}>
+                  <div className="card-inner">
+                    <div className="card-front">
+                      <img src={`/img/${image.name}`} alt="" />
+                    </div>
+                    <div className="card-back">
+                      <h1>{image.title}</h1>
+                      {Array.from(image.text).map((t, i) => (
+                        <p key={i}>{t}</p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div className="footer">
+        <h1>Честит Свети Валентин Вики</h1>
+        <h4>Надявам се да се видим скоро 💙</h4>
+      </div>
+    </>
   );
 }
 
